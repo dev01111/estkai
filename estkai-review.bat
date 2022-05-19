891c790344343ce87f6edae092a2416ea666dc65
dev01111 <105835627+dev01111@users.noreply.github.com>
2022-05-20 09:43:04 +1000

    eSTKai Reviews update.
    We changed some things, updated main "deta", added better reviewing mechanism.

---
+++ estkai-review.bat
@@ -0,0 +1,26 @@
+@echo off
+
+setlocal enabledelayedexpansion
+
+title eSTKai - Rewiewer
+
+
+set count=0
+
+for /f "tokens=*" %%x in (%APPDATA%\supertuxkart\addons\estkai-config.txt) do (
+    set /a count+=1
+    set var[!count!]=%%x
+)
+
+
+
+
+set /P name="Your name?"
+
+set /P review="Review for STK addon %2:"
+
+if exist "C:\estkai-files\%2-review-%name%.txt" ( del "C:\estkai-files\%2-review-%name%.txt" )
+
+echo %review%> "C:\estkai-files\%2-review-%name%.txt"
+
+xcopy /y /f "C:\estkai-files\%2-review-%name%.txt" "%var[1]%\%1\"
